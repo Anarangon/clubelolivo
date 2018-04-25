2a0edf56f724f86fa704a2ff0a123ae870a6fb20
angelnaranjogonzalez <angelnaranjogonzalez@192.168.0.42>
2018-04-25 18:57:48 +0200

    Subida inicial

---
+++ clubelolivo/src/main/java/com/salesianostriana/clubelolivo/ClubelolivoApplication.java
@@ -0,0 +1,12 @@
+package com.salesianostriana.clubelolivo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClubelolivoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClubelolivoApplication.class, args);
+	}
+}
